a1ca52516d89fe80c264d5aa0048c4d8b0f1b97f
Fullchee Zhang <fullchee@gmail.com>
2021-04-18 13:39:33 -0400

    03: Make the button text case insensitive

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -2,18 +2,20 @@
 // http://localhost:3000/counter
 
 import * as React from 'react'
-import {render, fireEvent, screen} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
+import userEvent from "@testing-library/user-event"
 import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  const {container} = render(<Counter />)
-  const increment = screen.getByRole('button', {name: 'Increment'})
-  const decrement = screen.getByRole('button', {name: 'Decrement'})
-  const message = container.firstChild.querySelector('div')
+  render(<Counter />)
+  const increment = screen.getByRole('button', {name: /Increment/i})
+  const decrement = screen.getByRole('button', {name: /Decrement/i})
+  const message = screen.getByText(/current count/i)
 
   expect(message).toHaveTextContent('Current count: 0')
-  fireEvent.click(increment)
+  userEvent.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
+  userEvent.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
+  
